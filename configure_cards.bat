9c16305298e2c6e88d7d315dffce98a8089a1874
taylor <tailor@u.washington.edu>
2013-11-22 10:45:05 -0800

    Whitespace cleanup
    Change-Id: If40db9f0ff00015af03d4a3543f7bcd1005c17cd

--- configure_cards.bat
+++ configure_cards.bat
@@ -7,7 +7,6 @@ nvspbind.exe /+ "Local Area Connection" ms_tcpip
 for %%G in ("Local Area Connection" "Local Area Connection 2") do (
 
     nvspbind.exe /e %%G ms_netbios
-
     nvspbind.exe /d %%G ms_pacer
     nvspbind.exe /d %%G ms_server
     nvspbind.exe /d %%G ms_tcpip6
